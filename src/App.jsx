6affa82e4b5744fd16719af89f31b6c9dc1aa163
Jelle <jelle@fashionunited.com>
2021-09-21 12:30:49 +0200

    update fashion news project

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import styled from '@emotion/styled';
-import { experimentalStyled as overrideMaterial } from '@material-ui/core/styles';
-import getNewsArticles from './lib/getNewsArticles';
-import MuiPagination from '@material-ui/core/Pagination';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import styled from "@emotion/styled";
+import { experimentalStyled as overrideMaterial } from "@material-ui/core/styles";
+import MuiPagination from "@material-ui/core/Pagination";
+import getNewsArticles from "./lib/getNewsArticles";
 
+const Pagination = ({ onChange, page, count }) => (
+  <>
+    <MuiPagination
+      count={count}
+      page={page}
+      color="primary"
+      onChange={onChange}
+    />
+  </>
+);
 
-const Pagination = ({onChange, page, count}) => {
-  return (
-    <>
-      <MuiPagination count={count} page={page} color="primary" onChange={onChange}/>
-    </>
-  );
-}
-
+Pagination.propTypes = {
+  page: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  count: PropTypes.number.isRequired,
+};
 
 const StyledCard = overrideMaterial(Card)`
   height: 100%;
@@ -34,7 +42,8 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
-const DEFAULT_IMAGE = 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg';
+const DEFAULT_IMAGE =
+  "https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg";
 
 const App = () => {
   const [data, setData] = useState({ newsArticles: [] });
@@ -43,7 +52,12 @@ const App = () => {
   const [offset, setOffset] = useState(0);
   const [page, setPage] = React.useState(1);
 
-  const { title: modalTitle, url: modalUrl, description: modalDescription, imageUrl: modalImageUrl } = modalArticle;
+  const {
+    title: modalTitle,
+    url: modalUrl,
+    description: modalDescription,
+    imageUrl: modalImageUrl,
+  } = modalArticle;
 
   const handleModal = (article) => {
     setModalArticle(article);
@@ -56,12 +70,12 @@ const App = () => {
 
   const handlePagination = (event, value) => {
     setPage(value);
-  }
+  };
 
   useEffect(() => {
     const variables = {
-      keywords: ['nike'],
-      offset: offset,
+      keywords: ["nike"],
+      offset,
     };
     const fetchArticles = async () => {
       const result = await getNewsArticles(variables);
@@ -78,59 +92,68 @@ const App = () => {
       <h1>Fashion News</h1>
       <Grid container spacing={3}>
         {data.newsArticles.map((newsArticle, index) => {
-
-         const { title, imageUrl } = newsArticle;
+          const { title, imageUrl } = newsArticle;
 
           return (
-          <Grid item xs={12} sm={3} key={imageUrl + index}>
-            <StyledCard>
-              <CardMedia
-                component="img"
-                alt={title}
-                image={imageUrl ? `https://r.fashionunited.com${imageUrl}` : DEFAULT_IMAGE}
-                title={title}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h6" component="h2">
-                  {title}
-                </Typography>
-                <Typography variant="button" color="textSecondary" component="div">
-                  {' '}
-                  <Button onClick={() => handleModal(newsArticle)}>Read more</Button>
-                </Typography>
-              </CardContent>
-            </StyledCard>
-          </Grid>
-          )
-})}
+            <Grid item xs={12} sm={3} key={imageUrl + index}>
+              <StyledCard>
+                <CardMedia
+                  component="img"
+                  alt={title}
+                  image={
+                    imageUrl
+                      ? `https://r.fashionunited.com${imageUrl}`
+                      : DEFAULT_IMAGE
+                  }
+                  title={title}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="h2">
+                    {title}
+                  </Typography>
+                  <Typography
+                    variant="button"
+                    color="textSecondary"
+                    component="div"
+                  >
+                    {" "}
+                    <Button onClick={() => handleModal(newsArticle)}>
+                      Read more
+                    </Button>
+                  </Typography>
+                </CardContent>
+              </StyledCard>
+            </Grid>
+          );
+        })}
       </Grid>
       <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-              >
-          <DialogTitle id="alert-dialog-title">
-            {modalTitle}
-          </DialogTitle>
-          <DialogContent>
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{modalTitle}</DialogTitle>
+        <DialogContent>
           <CardMedia
-                component="img"
-                alt={modalTitle}
-                image={modalImageUrl ? `https://r.fashionunited.com${modalImageUrl}` : DEFAULT_IMAGE}
-                title={modalTitle}
-              />
-            <DialogContentText id="alert-dialog-description">
-              {modalDescription}
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Disagree</Button>
-            <a href={modalUrl}>
-              Read more
-            </a>
-          </DialogActions>
-        </Dialog>
+            component="img"
+            alt={modalTitle}
+            image={
+              modalImageUrl
+                ? `https://r.fashionunited.com${modalImageUrl}`
+                : DEFAULT_IMAGE
+            }
+            title={modalTitle}
+          />
+          <DialogContentText id="alert-dialog-description">
+            {modalDescription}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <a href={modalUrl}>Read more</a>
+        </DialogActions>
+      </Dialog>
       <Pagination onChange={handlePagination} page={page} count={5} />
     </Container>
   );
